e9a53aed9f242150a265a869f7ac79bfaf771c4e
Kagisho Mogapi <mogapikagisho@gmail.com>
2023-09-12 16:30:26 +0200

    Add project files.

---
+++ BloodDonor/Model/Candidate.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodDonor.Model
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string Mobile { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        public byte Age { get; set; }
+        [Column(TypeName = "nvarchar(3)")]
+        public string BloodGroup { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+    }
+}
---
+++ BloodDonor/Model/DonorsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonor.Model
+{
+    public class DonorsDbContext : DbContext
+    {
+        public DonorsDbContext(DbContextOptions<DonorsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
